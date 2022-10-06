3dea63de7341fc03dc814f663007624e237842d0
george <115006794+thegeorgehewitt@users.noreply.github.com>
2022-10-06 10:51:03 +0100

    data

--- DataTypesProject/DataTypesProject/DataTypesProject.cpp
+++ DataTypesProject/DataTypesProject/DataTypesProject.cpp
@@ -1,20 +1,9 @@
-// DataTypesProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Size of char: " << sizeof(char) << " bytes" << std::endl;
+	std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
+	std::cout << "Size of short int: " << sizeof(short int) << " bytes" << std::endl;
+	std::cout << "Size of long int: " << sizeof(long int) << " bytes" << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
