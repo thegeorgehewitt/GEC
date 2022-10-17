45eb4524da707d4ffd444deead27ebf3f064ae35
george <115006794+thegeorgehewitt@users.noreply.github.com>
2022-10-17 13:19:42 +0100

    program 9 complete

---
+++ GEC_Sem1/Sem1_Week2/Program9/Program9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int mySavings = 2000;
+    int yourPercentage = 50;
+    int yourShare = mySavings * yourPercentage / 100;
+    
+    cout << "Your share: " << yourShare << endl;
+
+    return 0;
+}
