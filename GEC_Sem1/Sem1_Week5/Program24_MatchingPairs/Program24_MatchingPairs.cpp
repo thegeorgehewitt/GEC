4b70d3508234be0593d2dbe23628fc691abb3cf3
george <115006794+thegeorgehewitt@users.noreply.github.com>
2022-11-03 18:12:38 +0000

    program 24 basic complete

--- GEC_Sem1/Sem1_Week5/Program24_MatchingPairs/Program24_MatchingPairs.cpp
+++ GEC_Sem1/Sem1_Week5/Program24_MatchingPairs/Program24_MatchingPairs.cpp
@@ -5,31 +5,56 @@ using namespace std;
 void board();
 void guess(int a, int b);
 
-bool found[5] = { false };
-int count = 0;
+bool found[10] = { false };
+int guessCount = 0;
+char cardFaces[] = { 'B', 'D', 'C', 'A', 'E', 'B', 'A', 'D', 'C', 'E' };
 
 int main()
 {
-    char cardFaces[] = { 'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e' };
-    string cards[] = { "[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "[9]", "[10]" };
-    int score;
+    
+    int score = 0;
     bool playing = true;
+    int guess1;
+    int guess2;
 
     while (playing == true)
     {
-        int guess1;
-        int guess2;
+        board();
 
-        cout << cards[0, 4] << "\n" << cards[5, 9] << endl;
+        do
+        {
+            cout << "Pick a card" << endl;
+            cin >> guess1;
+            cout << "Pick another card" << endl;
+            cin >> guess2;
 
-        cout << "Pick a card" << endl;
-        cin >> guess1;
-        cout << "Pick another card" << endl;
-        cin >> guess2;
+            if (guess1 == guess2)
+                cout << "You need to pick two different cards." << endl;
+        } while (guess1 == guess2);
+        
+        if (cardFaces[(guess1-1)] == cardFaces[(guess2-1)])
+        {
+            score++;
+            found[guess1 - 1] = true;
+            found[guess2 - 1] = true;
+        }
 
+        if (cardFaces[(guess1 - 1)] != cardFaces[(guess2 - 1)])
+        {
+            guess(guess1, guess2);
+            system("pause");
+        }
 
-
-
+        if (score == 5)
+        {
+            cout << "You win!" << endl;
+            playing = false;
+        }
+        else
+        {
+            guessCount++;
+        }
+                
     }
 
 }
@@ -37,12 +62,46 @@ int main()
 void board()
 {
     system("cls");
-    cout << "You have made " << count << " guesses." << endl;
+    cout << "You have made " << guessCount << " guess(es)." << endl;
 
+    for (int i = 0; i < 10; i++)
+    {
+        if (found[i])
+        {
+            cout << "[" << cardFaces[i] << "]";
+        }
+        else
+        {
+            cout << "[" << i + 1 << "]";
+        }
 
+        if ((i == 4) || (i == 9))
+        {
+            cout << endl;
+        }
+    }
 }
 
 void guess(int a, int b)
 {
+    system("cls");
+    cout << "You have made " << guessCount << " guess(es)." << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if ((found[i]) || ((i == a - 1) || (i == b - 1)))
+        {
+            cout << "[" << cardFaces[i] << "]";
+        }
+        else
+        {
+            cout << "[" << i + 1 << "]";
+        }
+
+        if ((i == 4) || (i == 9))
+        {
+            cout << endl;
+        }
+    }
 
 }
