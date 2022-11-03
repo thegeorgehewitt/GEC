49d220f85aa89903ebd7b670c46560c099517192
george <115006794+thegeorgehewitt@users.noreply.github.com>
2022-11-03 14:56:19 +0000

    started 24

--- GEC_Sem1/Sem1_Week5/Program24_MatchingPairs/Program24_MatchingPairs.cpp
+++ GEC_Sem1/Sem1_Week5/Program24_MatchingPairs/Program24_MatchingPairs.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void board();
+void guess(int a, int b);
+
+bool found[5] = { false };
+int count = 0;
 
 int main()
 {
-    char cardFace[] = {"a", "b", "c", "d", }
+    char cardFaces[] = { 'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e' };
+    string cards[] = { "[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "[9]", "[10]" };
+    int score;
+    bool playing = true;
+
+    while (playing == true)
+    {
+        int guess1;
+        int guess2;
+
+        cout << cards[0, 4] << "\n" << cards[5, 9] << endl;
+
+        cout << "Pick a card" << endl;
+        cin >> guess1;
+        cout << "Pick another card" << endl;
+        cin >> guess2;
+
+
+
+
+    }
+
+}
+
+void board()
+{
+    system("cls");
+    cout << "You have made " << count << " guesses." << endl;
+
+
+}
+
+void guess(int a, int b)
+{
+
 }
