649d7ef15ed47a610e359f90a4f05adfe42e5d67
george <115006794+thegeorgehewitt@users.noreply.github.com>
2022-11-24 14:57:00 +0000

    program 37

--- GEC_Sem1/Sem1_Week8/Sem1_Week8/Program37_DesignatedUsername/Program37_DesignatedUsername.cpp
+++ GEC_Sem1/Sem1_Week8/Sem1_Week8/Program37_DesignatedUsername/Program37_DesignatedUsername.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-void newName(string userName);
+void newName(string& userName);
 
 int main()
 {
-    
+    string name;
+
+    cout << "Please input your name: " << endl;
+    getline(cin, name);
+
+    string& rName = name;
+
+    cout << "That username has been taken." << endl;
+
+    newName(rName);
+
+    cout << "Here's an alternative: " << name;
+}
+
+void newName(string& userName)
+{
+    srand(time(NULL));
+    int num = rand();
+    userName = userName + to_string(num);
 }
