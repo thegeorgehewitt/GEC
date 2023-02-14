1b73c7a15f179196952ff4e13af46fb52d714f8e
george <115006794+thegeorgehewitt@users.noreply.github.com>
2023-02-14 14:07:38 +0000

    added SDL framework

---
+++ GEC_Sem2/BaseMarioProject-main/BaseMarioProject-main/MarioBaseProject/Source.cpp
@@ -0,0 +1,10 @@
+//Be grateful for humble beginnings, because the next level will always require so much more of you
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+int main(int argc, char* args[])
+{
+	return 0;
+}
