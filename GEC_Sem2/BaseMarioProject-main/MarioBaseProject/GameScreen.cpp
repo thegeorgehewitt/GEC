0eb354b838c8832679991364175fc5b9ed7f2c4a
george <115006794+thegeorgehewitt@users.noreply.github.com>
2023-03-02 10:51:10 +0000

    Created GameScreen and GameScreenLevel1 Classes

---
+++ GEC_Sem2/BaseMarioProject-main/MarioBaseProject/GameScreen.cpp
@@ -0,0 +1,14 @@
+#include "GameScreen.h"
+
+GameScreen::GameScreen(SDL_Renderer* renderer)
+{
+	m_renderer = renderer;
+}
+GameScreen::~GameScreen()
+{
+	m_renderer = nullptr;
+}
+
+void GameScreen::Render() {}
+
+void GameScreen::Update(float deltaTime, SDL_Event e) {}
